452d5217b75554fc20de0960521396cc557ce9d4
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 19:39:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+ 
+  attr_accessor :color, :size, :color
+   attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand 
+  end
+  
+  
+  def cobble
+  puts 'The show has been repaired!'
+  end
+  
+  
+end
