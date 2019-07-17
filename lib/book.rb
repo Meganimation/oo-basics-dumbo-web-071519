d2e54dd26f359414862997a8282817664619ea7e
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 18:24:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,23 +1,12 @@
-class Book 
-  
-  def initialize(title)
-  end
-  
-  def title 
-    "And Then There Were None"
-  end
-  
-  def author=(author_name) 
-    
-  end
-  
-  
-end
+
 
 class Book 
     
       attr_accessor :title, :author, :page_count, :genre
-      
+        def title 
+    "And Then There Were None"
+  end
+  
 end
 
 
